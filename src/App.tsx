3709d16f796367f2f35f998edbf881a2cf463006
Melissa Rose <123melissa321@gmail.com>
2022-08-02 12:35:43 +0100

    Level-400 added show selector drop-down, case insensitive alphabetized

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,32 @@ import { EpisodeList } from "./episode-list";
 import { episodeMatch } from "./utils/episode-match";
 import IEpisode from "./utils/i-episode";
 import { useEffect, useState } from "react";
+import ShowList from './shows.json';
+import IShow from './utils/i-show';
+import caseInsensitiveAlphabet from "./utils/case-insensitive-alphabet";
 
 function App(): JSX.Element {
   const [searchText, setSearchText] = useState<string>("");
   const [previousData, setData] = useState<IEpisode[]>([]);
+  const [showURL, setShowURL] = useState<string>("https://api.tvmaze.com/shows/250/episodes");
   useEffect(() => {
-    fetch("https://api.tvmaze.com/shows/82/episodes")
+    fetch(showURL)
       .then((response) => response.json())
       .then((jsonBody: IEpisode[]) => {
         setData(jsonBody);
       });
-  }, []);
+  }, [showURL]);
   const filteredEpisodes: IEpisode[] = previousData.filter((episode) =>
     episodeMatch(episode, searchText)
   );
+
+  const sortedShowList = [...ShowList].sort(caseInsensitiveAlphabet)
   return (
     <>
+      <label htmlFor='shows'>Choose a show:</label>
+      <select name='shows' id='shows' onChange={(e) => setShowURL(e.target.value)}>
+        {sortedShowList.map((show: IShow, i) => <option key={i} value={`https://api.tvmaze.com/shows/${show.id}/episodes`}>{show.name}</option>)}
+      </select>
       <input
         value={searchText}
         onChange={(e) => setSearchText(e.target.value)}

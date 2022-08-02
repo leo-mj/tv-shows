f6d2e49a9df360b9451a2830d4aee650a6cc29a1
Melissa Rose <123melissa321@gmail.com>
2022-08-02 12:37:13 +0100

    Level-400 formatted and linted

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { EpisodeList } from "./episode-list";
 import { episodeMatch } from "./utils/episode-match";
 import IEpisode from "./utils/i-episode";
 import { useEffect, useState } from "react";
-import ShowList from './shows.json';
-import IShow from './utils/i-show';
+import ShowList from "./shows.json";
+import IShow from "./utils/i-show";
 import caseInsensitiveAlphabet from "./utils/case-insensitive-alphabet";
 
 function App(): JSX.Element {
   const [searchText, setSearchText] = useState<string>("");
   const [previousData, setData] = useState<IEpisode[]>([]);
-  const [showURL, setShowURL] = useState<string>("https://api.tvmaze.com/shows/250/episodes");
+  const [showURL, setShowURL] = useState<string>(
+    "https://api.tvmaze.com/shows/250/episodes"
+  );
   useEffect(() => {
     fetch(showURL)
       .then((response) => response.json())
@@ -22,12 +24,23 @@ function App(): JSX.Element {
     episodeMatch(episode, searchText)
   );
 
-  const sortedShowList = [...ShowList].sort(caseInsensitiveAlphabet)
+  const sortedShowList = [...ShowList].sort(caseInsensitiveAlphabet);
   return (
     <>
-      <label htmlFor='shows'>Choose a show:</label>
-      <select name='shows' id='shows' onChange={(e) => setShowURL(e.target.value)}>
-        {sortedShowList.map((show: IShow, i) => <option key={i} value={`https://api.tvmaze.com/shows/${show.id}/episodes`}>{show.name}</option>)}
+      <label htmlFor="shows">Choose a show:</label>
+      <select
+        name="shows"
+        id="shows"
+        onChange={(e) => setShowURL(e.target.value)}
+      >
+        {sortedShowList.map((show: IShow, i) => (
+          <option
+            key={i}
+            value={`https://api.tvmaze.com/shows/${show.id}/episodes`}
+          >
+            {show.name}
+          </option>
+        ))}
       </select>
       <input
         value={searchText}

e9999840a9c258005589120b3927bb0d86a886cc
Melissa Rose <123melissa321@gmail.com>
2022-08-05 11:22:34 +0100

    Removed allFavourites list

--- src/components/ShowList.tsx
+++ src/components/ShowList.tsx
@@ -1,55 +1,52 @@
 import IShow from "../utils/i-show";
 import { omitTags } from "../utils/omit-tags";
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
 interface ShowListProps {
   setSelectedShow: React.Dispatch<React.SetStateAction<number | null>>;
   show: IShow;
-  setAllFavourites: React.Dispatch<React.SetStateAction<number[]>>;
-  allFavourites: number[];
 }
 
 export default function ShowList({
   setSelectedShow,
-  setAllFavourites,
-  allFavourites,
   show,
 }: ShowListProps): JSX.Element {
-
   const [isFavourite, setIsFavourite] = useState<boolean>(() => {
     let value;
     try {
-      value = JSON.parse(window.localStorage.getItem(JSON.stringify(show.id)) || 'false');
-    }
-    catch (e) {
+      value = JSON.parse(
+        window.localStorage.getItem(JSON.stringify(show.id)) || "false"
+      );
+    } catch (e) {
       value = false;
     }
     return value;
   });
 
   useEffect(() => {
-    window.localStorage.setItem(JSON.stringify(show.id), JSON.stringify(isFavourite))
-  }, [isFavourite, show.id])
+    window.localStorage.setItem(
+      JSON.stringify(show.id),
+      JSON.stringify(isFavourite)
+    );
+  }, [isFavourite, show.id]);
 
   return (
     <div className="showEntry">
       <h1 onClick={() => setSelectedShow(show.id)}>{show.name}</h1>
-      {(isFavourite) || allFavourites.includes(show.id) ? (
+      {isFavourite ? (
         <button
           onClick={() => {
-            setAllFavourites([...allFavourites].filter((a) => a !== show.id))
             setIsFavourite(false);
-            
-          }
-          }
+          }}
         >
           ★
         </button>
       ) : (
-        <button onClick={() => {
-          setAllFavourites([...allFavourites, show.id])
-          setIsFavourite(true);
-        }}>
+        <button
+          onClick={() => {
+            setIsFavourite(true);
+          }}
+        >
           ☆
         </button>
       )}
